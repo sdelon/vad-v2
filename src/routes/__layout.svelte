026882a77ef3f0c9085ab3bc139b2383f6714c5e
sdelon <delon.steph@gmail.com>
2021-07-21 14:32:55 +0200

    wip

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,9 @@
 <script>
   import '../app.postcss'
-  import { onMount } from 'svelte'
   import Header from '$lib/Header.svelte'
   import Footer from '$lib/Footer.svelte'
   import FacebookSVG from '../../static/images/icon-library.svg#icon-facebook'
   import logoVAD from '../../static/images/logo-arbre-VAD-v2.svg'
-
-	onMount(() => {
-      import('https://unpkg.com/agnostic-axe@3').then(({ AxeObserver, logViolations }) => {
-        const MyAxeObserver = new AxeObserver(logViolations)
-        MyAxeObserver.observe(document)
-      })
-    })
 </script>
 
 <Header
